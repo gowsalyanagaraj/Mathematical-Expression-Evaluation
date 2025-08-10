dc724c7dcaa5945fc0ee5491aab3962628bc8d4b
Gowsalya Nagaraj <gowsalyanagaraj07@gmail.com>
2025-08-10 19:58:29 +0530

    Add files via upload

---
+++ MathematicalExpression/MathematicalExpression.Test/MathematicalExpressionTest.cs
@@ -0,0 +1,33 @@
+﻿namespace MathematicalExpression.Test;
+
+[TestClass]
+public class MathematicalExpressionTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // Arrange
+        MathematicalExpression solution = new MathematicalExpression();
+        string expression = "3 + (2 * 4) - 5 / (1 + 1)";
+
+        // Act
+        double result = solution.Solution(expression);
+
+        // Assert
+        Assert.AreEqual(result, 8.5);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        // Arrange
+        MathematicalExpression solution = new MathematicalExpression();
+        string expression = "3 + (2 * 4) - 5 / ((6 / 6) + 1)";
+
+        // Act
+        double result = solution.Solution(expression);
+
+        // Assert
+        Assert.AreEqual(8.5, result);
+    }
+}
---
+++ MathematicalExpression/MathematicalExpression/MathematicalExpression.cs
@@ -0,0 +1,92 @@
+﻿namespace MathematicalExpression;
+
+public class MathematicalExpression
+{
+    public static string subExpression;
+    public double Solution(string expression)
+    {
+        var tempValues = expression.ToCharArray().ToList();
+
+        tempValues.RemoveAll(x => x == ' ');
+
+        List<string> values = SplitValues(tempValues);
+        SplitExpression(values, 0);
+        string evaluatedResult = EvaluateExpression(values);
+        
+        return Convert.ToDouble(evaluatedResult);
+    }
+    private static List<string> SplitValues(List<char> chars)
+    {
+        List<string> values = new List<string>();
+        foreach(char c in chars)
+        {
+            values.Add(c.ToString());
+        }
+        return values;
+    }
+    private static void SplitExpression(List<string> values, int i)
+    {
+        subExpression = "";
+        while (i < values.Count)
+        {
+            if (values[i].Equals("("))
+            {
+                int j = i + 1;
+                while(j < values.Count)
+                {
+                    if (values[j].Equals("("))
+                    {
+                        SplitExpression(values, j);
+                    }
+                    else if (values[j].Equals(")"))
+                    {
+                        values[i] = EvaluateExpression(SplitValues(subExpression.ToCharArray().ToList()));
+                        values.RemoveAt(j);
+                        break;
+                    }
+                    subExpression += values[j];
+                    values.RemoveAt(j);
+                }
+                subExpression = "";
+            }
+            i++;
+        }
+    }
+    
+    private static string EvaluateExpression(List<string> expression)
+    {
+        while(expression.Count != 1)
+        {
+            if (expression.Contains("/"))
+            {
+                int index = expression.FindIndex(x => x == "/");
+                expression[index - 1] = (Convert.ToDouble(expression[index - 1]) / Convert.ToDouble(expression[index + 1])).ToString();
+                expression.RemoveAt(index);
+                expression.RemoveAt(index);
+            }
+            else if (expression.Contains("*"))
+            {
+                int index = expression.FindIndex(x => x == "*");
+                expression[index - 1] = (Convert.ToDouble(expression[index - 1]) * Convert.ToDouble(expression[index + 1])).ToString();
+                expression.RemoveAt(index);
+                expression.RemoveAt(index);
+            }
+            else if (expression.Contains("+"))
+            {
+                int index = expression.FindIndex(x => x == "+");
+                expression[index - 1] = (Convert.ToDouble(expression[index - 1]) + Convert.ToDouble(expression[index + 1])).ToString();
+                expression.RemoveAt(index);
+                expression.RemoveAt(index);
+            }
+            else if (expression.Contains("-"))
+            {
+                int index = expression.FindIndex(x => x == "-");
+                expression[index - 1] = (Convert.ToDouble(expression[index - 1]) - Convert.ToDouble(expression[index + 1])).ToString();
+                expression.RemoveAt(index);
+                expression.RemoveAt(index);
+            }
+        }
+
+        return expression[0]; 
+    }
+}
